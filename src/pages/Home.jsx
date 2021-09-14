d0585f7fa67569f432ae968db12e0b21ba822207
exodojaffar <exodojaffar@gmail.com>
2021-09-13 22:28:58 -0400

    add start up source

---
+++ src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './Home.css';
+
+import firebaseConfig from "../FirebaseConfig.json";
+import { initializeHome } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Initialize Firebase
+const app = initializeHome(firebaseConfig);
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <img src={logo} className="Home-logo" alt="logo" />
+        <p>
+          Edit <code>src/Home.js</code> and save to reload.
+        </p>
+        <a
+          className="Home-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
