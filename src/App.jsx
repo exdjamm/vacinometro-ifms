54a4a64a8d29a34e0dadbfc7f22f551171e9f314
exodojaffar <exodojaffar@gmail.com>
2021-09-17 19:40:16 -0400

    update: use hash route for pages
    - Add a route for Home page and UpdateVacina page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+// import { Routes } from "../routes";
+
 import Home from "./pages/Home";
+import UpdateVacina from "./pages/UpdateVacina";
+
+function Router({component: Component, ...rest}) {
+  return (
+    <Route {...rest} render={props => (<> <Component {... props}/> </> )}/>
+  )
+
+}
 
 function App() {
   return (
-    <Home/>
+    <Switch>
+      <Router exact path="/" component={Home}/>
+      <Router exact path="/vacinas" component={UpdateVacina}/>
+    </Switch>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>,
+  </HashRouter>,
   document.getElementById('root')
 );
