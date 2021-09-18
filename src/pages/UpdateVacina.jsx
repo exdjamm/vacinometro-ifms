176d07f37673e62faee4d8204b6a16ebdf9f1921
exodojaffar <exodojaffar@gmail.com>
2021-09-17 20:30:00 -0400

    create: UpdateVacina page
    To user update their dose of vaccine

---
+++ src/pages/UpdateVacina.jsx
@@ -0,0 +1,24 @@
+import "UpdateVacina.css";
+
+function UpdateVacina() {
+  return (
+    <div className="UpdateVacina">
+      <header className="UpdateVacina-header">
+        <img src={logo} className="UpdateVacina-logo" alt="logo" />
+        <p>
+          Edit <code>src/UpdateVacina.js</code> and save to reload.
+        </p>
+        <a
+          className="UpdateVacina-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default UpdateVacina;
