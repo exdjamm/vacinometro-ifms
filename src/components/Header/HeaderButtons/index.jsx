ce5029c9c28515cc07cb99a6399a7e52003583b1
exodojaffar <exodojaffar@gmail.com>
2021-09-19 19:38:31 -0400

    update: able to pass className in props

--- src/components/Header/HeaderButtons/index.jsx
+++ src/components/Header/HeaderButtons/index.jsx
@@ -2,7 +2,7 @@ import "bootstrap-icons/font/bootstrap-icons.css"
 
 import "./index.css";
 
-function HeaderButton({color='green', text='Acessar Conta'}){
+function HeaderButton({color='green', text='Acessar Conta', className=''}){
 	const isColorWhite = color == "white";
 
 	const styleGreen = {
@@ -18,10 +18,9 @@ function HeaderButton({color='green', text='Acessar Conta'}){
 	const style = (isColorWhite)? styleWhite : styleGreen;
 
 	return (
-		<div className="header-btns" style={style}>
+		<div className={`${className} header-btns`} style={style}>
 			<span className="header-btn-content ubuntu-font">{text}</span>
 			<i hidden={isColorWhite} className="bi bi-box-arrow-in-right btn-icon"></i>
-
 		</div>
 	);
 }
