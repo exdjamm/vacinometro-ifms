d032312280a60cceeffdb1b20b43822c03577974
exodojaffar <exodojaffar@gmail.com>
2021-09-19 19:39:15 -0400

    update: done header of page with a hidden 'login' button
    - Maybe have questionable code for styling

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,22 @@
+import HeaderTitle from './HeaderTitle'
+import HeaderButton from './HeaderButtons'
+
+
 import "./index.css";
 
 function RenameThis(props){
 
 	return (
-		<></>	
+		<header id="page-header">
+			<HeaderTitle/>
+
+			<div className="hbuttons">
+				<HeaderButton className="hlogin-btn" color="white" text="Login"/>
+				<HeaderButton className="haccess-btn" color="green" text="Acessar Conta"/>
+			</div>
+				
+			
+		</header>
 	);
 }
 
