0198df8aa601dff6822080ba0d9e97c00ee7f61c
exodojaffar <exodojaffar@gmail.com>
2021-09-18 20:45:25 -0400

    update: create button code and style of header
    it has two style of buttons, one dark-green and another white

--- src/components/Header/HeaderButtons/index.jsx
+++ src/components/Header/HeaderButtons/index.jsx
@@ -1,9 +1,28 @@
+import "bootstrap-icons/font/bootstrap-icons.css"
+
 import "./index.css";
 
-function RenameThis(props){
+function RenameThis({color}){
+	const isColorWhite = color == "white";
+
+	const styleGreen = {
+		color: "var(--white)",
+		backgroundColor: "var(--dark-green)"
+	}
+
+	const styleWhite = {
+		color: "var(--dark-green)",
+		backgroundColor: "var(--white)"
+	}
+
+	const style = (isColorWhite)? styleWhite : styleGreen;
 
 	return (
-		<></>	
+		<div className="header-btns" style={style}>
+			<span className="header-btn-content ubuntu-font">Acessar Conta</span>
+			<i hidden={isColorWhite} className="bi bi-box-arrow-in-right btn-icon"></i>
+
+		</div>
 	);
 }
 
