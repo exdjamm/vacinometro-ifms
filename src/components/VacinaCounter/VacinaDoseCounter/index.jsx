6302c467c674e438e34ad3ac8bd65232bb566008
exodojaffar <exodojaffar@gmail.com>
2021-09-19 21:57:31 -0400

    update: create vacina counter card
    It has 3 props:
     - doseValue -> How many doses has been taken
     - doseTitle -> Name of dose
     - doseSubTitle -> Sub title of the name, case need

--- src/components/VacinaCounter/VacinaDoseCounter/index.jsx
+++ src/components/VacinaCounter/VacinaDoseCounter/index.jsx
@@ -1,9 +1,18 @@
 import "./index.css";
 
-function RenameThis(props){
+function RenameThis({doseValue=0, doseTitle='1ª Dose', doseSubTitle=''}){
+	const hasSubTitle = doseSubTitle == '';
 
 	return (
-		<></>	
+		<article className="vacina-dcounter">
+			<div className="dose-title">
+				<span className="d-maintitle poppins-font">{doseTitle}</span>
+				<span className="d-subtitle poppins-font" hidden={hasSubTitle}>{doseSubTitle}</span>
+			</div>
+			<div className="dose-number">
+				<span className="poppins-font">{doseValue}</span>
+			</div>
+		</article>	
 	);
 }
 
