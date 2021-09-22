971d305884ff9750ac4a5ae44d76ad5675ba5734
exodojaffar <exodojaffar@gmail.com>
2021-09-21 20:33:44 -0400

    create: disclaimer warn component
    Pode ter problemas com a escala dos dispositivo e problemas futuros com portaçao para desktop

--- src/components/DisclaimerWarn/index.jsx
+++ src/components/DisclaimerWarn/index.jsx
@@ -2,7 +2,12 @@ import "./index.css";
 
 function RenameThis(props) {
 	return (
-		<></>
+		<article className="disclaimer-container">
+			<i className='bi bi-exclamation-circle'></i>
+			<span className="poppins-font">
+				Este site não é uma página oficial do IFMS e os dados aqui presentes são fornecidos por alunos do Instituto, podendo não representar o valor real de estudantes vacinados.
+			</span>
+		</article>
 	);
 }
 
