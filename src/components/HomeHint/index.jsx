79dd42d9f7c57579e800df46ac95a15b2250d094
exodojaffar <exodojaffar@gmail.com>
2021-09-21 20:16:35 -0400

    create: home hint component com a estilização
    Ainda ha coisa no CSS que terão que mudam, e que poderão dificultar da modularização para desktop

--- src/components/HomeHint/index.jsx
+++ src/components/HomeHint/index.jsx
@@ -1,8 +1,16 @@
 import "./index.css";
 
-function RenameThis(props) {
+function RenameThis() {
 	return (
-		<></>
+		<div className="home-hint-container">
+			<span className="hhint-text poppins-font">
+				Cadastra-se para registrar sua vacinação ou faça login para atualizar seus dados vacinais.
+				</span>
+			<button className="hhint-button poppins-font">
+				<span>Contribuir com o Vacinômetro</span>
+				<i className="bi bi-arrow-right-circle-fill"></i>
+			</button>
+		</div>
 	);
 }
 
