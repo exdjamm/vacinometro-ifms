5adeb7c417675e08699aa90ff347859bbcf5d023
exodojaffar <exodojaffar@gmail.com>
2021-09-24 22:50:21 -0400

    feat: implementa o context do firebase para logged
    Usa para modificar o texto mostrado caso logado ou não.
    Tambem coloca na props a função que será chamada quando clicado, poderá ser substituido por um contextPage talvez

--- src/components/HomeHint/index.jsx
+++ src/components/HomeHint/index.jsx
@@ -1,13 +1,22 @@
+import {useState, useContext} from 'react';
+import FirebaseContext from '../../contexts/FirebaseContext';
+
 import "./index.css";
 
-function HomeHint() {
+function HomeHint({ handleAccessProfile }) {
+	const {user} = useContext(FirebaseContext);
+	const logged = user != undefined;
+
+	const textHint = (!logged) ? "Cadastre-se para registrar sua vacinação ou faça login para atualizar seus dados vacinais." : "Acesse seu perfil para atualizar seus dados";
+	const textHintButton = (!logged) ? "Contribuir com o Vacinômetro" :"Atualizar meus dados"
+
 	return (
 		<div className="home-hint-container">
 			<span className="hhint-text poppins-font">
-				Cadastre-se para registrar sua vacinação ou faça login para atualizar seus dados vacinais.
-				</span>
-			<button className="hhint-button poppins-font">
-				<span>Contribuir com o Vacinômetro</span>
+				{textHint}
+			</span>
+			<button onClick={handleAccessProfile} className="hhint-button poppins-font">
+				<span>{textHintButton}</span>
 				<i className="bi bi-arrow-right-circle-fill"></i>
 			</button>
 		</div>
