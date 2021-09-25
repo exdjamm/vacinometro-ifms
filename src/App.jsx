f6932fe79a99dacd659ff7a94629bb9191b75435
exodojaffar <exodojaffar@gmail.com>
2021-09-24 22:40:19 -0400

    feat: contexto do firebase por todo web app
    Implementa o contexto do firebase passando o auth, db, app e user hook para todas as paginas.

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,43 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import Home from "./pages/Home";
 import UpdateVacina from "./pages/UpdateVacina";
 
-function Router({ component: Component, ...rest }) {
+import FirebaseContext from './contexts/FirebaseContext';
+
+import firebaseConfig from "./FirebaseConfig.json";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+const db = getFirestore(app);
+
+const firebaseContext = {
+  app: app,
+  auth: auth,
+  db: db
+}
+
+function Router({component: Component, ...rest}) {
+ 	const [user] = useAuthState(auth)
+ 	
+ 	const Render = (props) => {
+ 		
+
+		return (
+			<FirebaseContext.Provider value={{...firebaseContext, user}}>
+				<> 
+					<Component {... props}/> 
+				</>   
+			</FirebaseContext.Provider>
+		)
+	}
+
 	return (
-		<Route {...rest} render={props => (<> <Component {...props} /> </>)} />
+		<Route {...rest} render={Render}/>
 	)
 
 }
@@ -14,8 +48,8 @@ function Router({ component: Component, ...rest }) {
 function App() {
 	return (
 		<Switch>
-			<Router exact path="/" component={Home} />
-			<Router exact path="/vacinas" component={UpdateVacina} />
+			<Router exact path="/" component={Home}/>
+			<Router exact path="/vacinas" component={UpdateVacina}/>
 		</Switch>
 	);
 }
