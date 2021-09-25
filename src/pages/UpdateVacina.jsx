ee880461ef3442695a6d9ecbfbb81221f9035c5f
exodojaffar <exodojaffar@gmail.com>
2021-09-24 22:47:54 -0400

    feat: usa contexto da pagina para header
    Usa o context para manipular estados de icone e textos do header.
    Tambem exclui o uso do firebase, pois esta sendo usado no context do App.jsx

--- src/pages/UpdateVacina.jsx
+++ src/pages/UpdateVacina.jsx
@@ -1,16 +1,35 @@
+import {useState} from 'react';
+
 import "./UpdateVacina.css";
 
 import Header from '../components/Header';
 import Profile from '../components/Profile';
 import CopyrightFooter from '../components/CopyrightFooter';
 
+import PageContext from '../contexts/PageContext';
+
+const pageContext = {
+	iconsHeader:{
+		logged: "house",
+		notLogged: "box-arrow-in-right"
+	},
+	textHeader:{
+		mainButtonLogOff: "Cadastro",
+		mainButtonLogOn: "Tela inicial",
+		mobileButton: "Tela inicial"
+	}
+}
+
 function UpdateVacina() {
+
 	return (
-		<div id="update-vacina-page">
-			<Header />
-			<Profile />
-			<CopyrightFooter />
-		</div>
+		<PageContext.Provider value={pageContext}>
+			<div id="update-vacina-page">
+				<Header />
+				<Profile />
+				<CopyrightFooter />
+			</div>
+		</PageContext.Provider>
 	);
 }
 
