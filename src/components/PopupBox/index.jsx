7259525de848ebc7a76e5bbcb8f2a1c6c5f8e5d8
exodojaffar <exodojaffar@gmail.com>
2021-10-05 16:52:36 -0400

    create: popupbox component
    Aos trancos e barrancos, mas cria

--- src/components/PopupBox/index.jsx
+++ src/components/PopupBox/index.jsx
@@ -1,8 +1,31 @@
+import PopupHint from './PopupHint'
+import PopupWarn from './PopupWarn'
+
 import "./index.css";
 
-function RenameThis(props) {
+function RenameThis({ button: Component, mainIcon = "shield-lock", warnText = "", hintText = "", showWarn = false, closeButton }) {
 	return (
-		<></>
+		<div id="background-popup">
+			<section className="popup-box">
+				<header className="pp-header">
+					<i className={`bi bi-${mainIcon} pp-main-icon`}/>
+					<button onClick={closeButton} className="pp-btn-close">
+						<i className="bi bi-x-circle"/>
+					</button>
+				</header>
+
+				<div className="pp-content">
+					<div className="pp-text">
+						<PopupWarn warnText={warnText} hidden={showWarn} />
+						<PopupHint hintText={hintText} />	
+					</div>
+					
+					<div className="pp-button">
+						<Component />
+					</div>
+				</div>
+			</section>
+		</div>
 	);
 }
 
