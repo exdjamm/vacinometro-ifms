a4bb94001bb214807b9511bffc581e8f891abd15
exodojaffar <exodojaffar@gmail.com>
2021-10-05 16:52:11 -0400

    update: pass rest on component

--- src/components/PopupBox/PopupWarn/index.jsx
+++ src/components/PopupBox/PopupWarn/index.jsx
@@ -1,8 +1,8 @@
 import "./index.css";
 
-function RenameThis({ warnText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."}) {
+function RenameThis({ warnText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", ...rest}) {
 	return (
-		<span className="poppins-font popup-warn">
+		<span className="poppins-font popup-warn" { ...rest }>
 			{warnText}			
 		</span>
 	);
