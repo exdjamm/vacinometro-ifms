56996a06a57082849f6f9fa88dec0db07defd747
exodojaffar <exodojaffar@gmail.com>
2021-10-05 16:53:13 -0400

    create: Login Popup component

--- src/components/LoginPopup/index.jsx
+++ src/components/LoginPopup/index.jsx
@@ -1,9 +1,23 @@
+import LoginGoogleButton from './LoginGoogleButton'
+import PopupBox from "../PopupBox"
+
 import "./index.css";
 
-function RenameThis(props) {
+function LoginPopup({state: [state, setState]}) {
+	const closePopup = () => {
+		setState(true)
+	}
+
 	return (
-		<></>
+		<article hidden={state}>
+			<PopupBox 
+				warnText="Erro no Login! Tente novamente"
+				hintText="Use sua conta instituional para criar ou acessar seu perfil."
+				closeButton={closePopup}
+				button={LoginGoogleButton} 
+			/>
+		</article>
 	);
 }
 
-export default RenameThis;
+export default LoginPopup;
